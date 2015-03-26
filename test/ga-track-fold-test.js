076cca8709f9f4e3642a291daa3570d3ae6ef2ce
Eric Willis <eric@ecwillis.com>
2015-03-26 10:16:41 -0700

    Updating tests with proper tests

--- test/ga-track-fold-test.js
+++ test/ga-track-fold-test.js
@@ -1,6 +1,6 @@
 //ga stub
 window._gaq = {
-  data: null,
+  data: [],
   clear: function() {
     this.data = [];
   },
@@ -23,6 +23,41 @@ suite('ga-track', function() {
 
   test('api exists', function() {
     assert.equal(typeof $.gaTrackFold, 'function');
+  });
+
+  test('it pushes the correct values to gaTrack', function() {
+    // Setup
+    window._gaq.clear();
+
+    // Perform
+    $.gaTrackFold();
+    
+    // Test
+    var data = window._gaq.data;
+    assert.equal(data.length, 4);
+    
+    assert.equal(data[0][0], '_trackEvent');
+    assert.equal(data[0][1], 'Viewport');
+    assert.equal(data[0][2], 'Pixel Ratio');
+
+    assert.equal(data[1][0], '_trackEvent');
+    assert.equal(data[1][1], 'Viewport');
+    assert.equal(data[1][2], 'Width');
+
+    assert.equal(data[2][0], '_trackEvent');
+    assert.equal(data[2][1], 'Viewport');
+    assert.equal(data[2][2], 'Height');
+
+    assert.equal(data[3][0], '_trackEvent');
+    assert.equal(data[3][1], 'Viewport');
+    assert.equal(data[3][2], 'Size');
+  });
+
+  test('it sets the proper cookie',function() {
+    assert.equal(typeof monster, 'object');
+    assert.equal(typeof monster.get, 'function');
+    
+    assert.equal(monster.get('gaTrackCookie'), 1);
   });
 
 });
