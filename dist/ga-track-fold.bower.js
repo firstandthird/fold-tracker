d5ea7a4e4ce4a37a3977de6801e168d183cadf06
Eric Willis <eric@ecwillis.com>
2015-03-26 08:59:54 -0700

    Rough cut

--- dist/ga-track-fold.bower.js
+++ dist/ga-track-fold.bower.js
@@ -7,15 +7,35 @@
 */
 ;(function($) {
 
-  $.gaTrackFold = function() {
+  $.gaTrackFold = function(options) {
+    var opt = $.extend({}, $.gaTrackFold.defaults, options);
+    var cookieName = opt.cookieName | $.gaTrackFold.defaults.cookieName;
+
     if (typeof $.gaTrack === 'undefined') {
       return this;
     }
 
-    var pixelRatio = window.devicePixelRatio;
+    console.log(monster.get(cookieName));
+    if(monster.get(cookieName) === null)
+    {
+      var pixelRatio = window.devicePixelRatio || 1;
+      $.gaTrack('Viewport', 'Pixel Ratio', pixelRatio);
 
-    console.log("PR: " + pixelRatio);
+      var viewWidth = window.innerWidth || 0;
+      $.gaTrack('Viewport', 'Width', viewWidth);
 
+      var viewHeight = window.innerHeight || 0;
+      $.gaTrack('Viewport', 'Height', viewHeight);
+
+      $.gaTrack('Viewport', 'Size', viewWidth + 'x' + viewHeight);
+
+      monster.set(cookieName, '1'); 
+    }
+
+  };
+
+  $.gaTrackFold.defaults = {
+    cookieName: 'gaTrackCookie'
   };
   
 })(jQuery);
