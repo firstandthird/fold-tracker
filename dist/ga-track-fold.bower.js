d48bd694391104644dd914d6e86984b00254a03a
Eric Willis <eric@ecwillis.com>
2015-03-25 17:02:36 -0700

    Initial project setup with base files, etc.

---
+++ dist/ga-track-fold.bower.js
@@ -0,0 +1,10 @@
+/*!
+ * ga-track-fold - A tracking library for tracking viewports
+ * v0.0.0
+ * https://github.com/firstandthird/ga-track-fold
+ * copyright  2015
+ * MIT License
+*/
+;(function($) {
+  console.log('The Thug Life');
+})(jQuery);
